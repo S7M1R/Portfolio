d624d0292ed4926c22faf134f304290a34250fa8
S7M1R <sameeroddinkazi2002@gmail.com>
2024-03-04 15:13:25 +0530

    UPDATE: PingTask render app for every 29 min

---
+++ src/main/java/com/Portfolio/util/PintTask.java
@@ -0,0 +1,21 @@
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class PingTask {
+
+    private final String WebsiteUrl = "https://sameeroddinkazi.onrender.com";
+
+    private final RestTemplate restTemplate;
+
+    public PingTask(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Scheduled(cron = "0 */29 6-2 * * *") // Scheduled every 29 minutes from 6 am to 2 am, 7 days a week
+    public void pingWebsite() {
+        System.out.println("Pinging website to prevent inactivity...");
+        restTemplate.getForObject(WebsiteUrl, String.class);
+    }
+}
